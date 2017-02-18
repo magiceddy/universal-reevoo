0a06fac8bc43f7d52758b475163311c9a1d33025
Rinaldo <rrossi@byte-code.com>
2017-02-18 14:18:01 +0100

    add Products contract and update migration

---
+++ contracts/Products.sol
@@ -0,0 +1,68 @@
+pragma solidity^0.4.7;
+
+/// @title List of product
+/// @author Rinaldo Rossi <rinaldo.rossi.web@gmail.com>
+
+contract Products {
+
+    /* emit when product is added */
+    event productAdded(
+        uint indexed code,
+        string name
+    );
+
+    /* emit when product is updated */
+    event productUpdated(
+        uint indexed code,
+        string indexed name
+    );
+
+    struct Product {
+        string name;
+        bool created;
+    }
+
+    mapping (uint => Product) productsList;
+
+    /// @dev check if product whas already registered
+    /// @param _code - product code
+    modifier productAlreadyCreated(uint _code) {
+        if(productsList[_code].created) 
+            throw;
+        _;
+    }
+
+    /// @dev check if product was not created
+    /// @param _code product code
+    modifier productNotCreated(uint _code) {
+        if(!productsList[_code].created) 
+            throw;
+        _;
+    }
+
+    /// check if product code is valide
+    /// @param _code - product code
+    modifier valideCode(uint _code) {
+        if(_code <= 0) throw;
+        _;
+    }
+
+    function Products () {}
+
+    /* @dev add product for specific company
+     * @param _code - product code
+     * @param _name - product name 
+     */
+    function addProduct(uint _code, string _name) 
+        valideCode(_code) 
+        productAlreadyCreated(_code) 
+    {
+        productsList[_code] = Product(
+            {
+                name: _name, 
+                created: true
+            }
+        );
+        productAdded(_code, _name);
+    }
+}
