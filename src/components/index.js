76a8202c876846a810a50481aabbb1bb4253f358
Rinaldo <rrossi@byte-code.com>
2017-03-08 21:34:49 +0100

    temporary components implementation

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import App from './App';
+import Dashboard from './Dashboard';
+
+module.exports = {
+  App,
+  Dashboard
+};
--- src/login/LoginButton.js
+++ src/login/LoginButton.js
@@ -1,7 +1,7 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
 
 // Images
-import uPortLogo from '../img/uport-logo.svg';
+/*import uPortLogo from '../img/uport-logo.svg';
 
 const LoginButton = ({ onLoginUserClick }) => {
   return(
@@ -9,6 +9,25 @@ const LoginButton = ({ onLoginUserClick }) => {
       <a href="#" className="pure-menu-link" onClick={(event) => onLoginUserClick(event)}><img className="uport-logo" src={uPortLogo} alt="UPort Logo" />Login with UPort</a>
     </li>
   );
+};
+
+export default LoginButton;*/
+
+class LoginButton extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    console.log(this.context);
+    return <div>Ciao</div>;
+  }
+}
+
+LoginButton.contextTypes = {
+  contracts: React.PropTypes.object,
+  web3: React.PropTypes.object
 };
 
 export default LoginButton;
