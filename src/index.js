202c7ea97725f1f5558eb99cab045bcb23359777
Rinaldo <rrossi@byte-code.com>
2017-03-08 22:03:52 +0100

    add material-ui && redux-form && react-tap-event-plugin && babel-preset-stage-1

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import { syncHistoryWithStore } from 'react-router-redux';
 import { Provider } from 'react-redux';
 import { getRoutes } from './routes';
 import store from './store';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+injectTapEventPlugin();
 
 
 const history = syncHistoryWithStore(browserHistory, store);
