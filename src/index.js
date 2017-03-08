0735f553545566b012f95e5242adc1a48f8f9177
Rinaldo <rrossi@byte-code.com>
2017-03-08 22:24:00 +0100

    add MuiThemProvider

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import { Provider } from 'react-redux';
 import { getRoutes } from './routes';
 import store from './store';
 import injectTapEventPlugin from 'react-tap-event-plugin';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 injectTapEventPlugin();
 
@@ -13,11 +14,13 @@ injectTapEventPlugin();
 const history = syncHistoryWithStore(browserHistory, store);
 
 ReactDOM.render((
+  <MuiThemeProvider>
     <Provider store={store}>
       <Router history={history}>
         {getRoutes}
       </Router>
     </Provider>
+  </MuiThemeProvider>
   ),
   document.getElementById('root')
 );
