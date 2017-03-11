48054d41239a534a28931287f763f0d07aa8cc39
Rinaldo <rrossi@byte-code.com>
2017-03-11 23:21:44 +0100

    add show modal and show loading logic

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,58 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
+import {connect} from 'react-redux';
 import {default as buildedContracts} from '../contracts';
 import ContractProvider from './ContractProvider';
+import { isLoading} from '../components/Loader/selector';
+import { closeModal } from '../components/Modal/action';
+import { getModalStatus } from '../components/Modal/selector';
+import { CircularProgress } from 'material-ui';
+import ModalDialog from '../components/Modal/modalDialog';
 
 class App extends Component {
+
+  showModal() {
+    const status = this.props.modal;
+    return status ? <ModalDialog open={status} onClose={this.props.closeModal} /> : null;
+  }
+
+  showLoader() {
+    const modalStyle = {
+      'position': 'absolute', 
+      'top': '50%', 
+      'right': '50%'
+    };
+
+    return (
+      this.props.isLoading ?
+        <CircularProgress
+          style={modalStyle}
+          size={60}
+          thickness={7}
+        /> : null
+    );
+  }
+
   render() {
     return (
       <ContractProvider contracts={buildedContracts}>
         {this.props.children}
+        {this.showLoader()}
+        {this.showModal()}
       </ContractProvider>
     );
   }
 }
 
-export default App;
+App.propTypes = {
+  isLoading: PropTypes.bool,
+  modal: PropTypes.bool,
+  closeModal: PropTypes.func
+};
+
+export default connect(
+  (state) => ({
+    isLoading: isLoading(state),
+    modal: getModalStatus(state)
+  }),
+  {closeModal}
+)(App);
